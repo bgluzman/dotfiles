488ff6bb72ad5b7bb0fe23f2f8e150da0c122a38
Brian Gluzman <bgluzman@gmail.com>
2023-05-18 13:47:22 -0400

    bindings for eglot mode and easier elisp editing packages

--- emacs/init.el
+++ emacs/init.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(modus-themes lsp-ui lsp-mode yasnippet company magit which-key ace-window)))
+   '(rainbow-delimiters paredit modus-themes lsp-ui lsp-mode yasnippet company magit which-key ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -17,15 +17,37 @@
   :config
   ;; completion
   (fido-vertical-mode)
+  ;; always use electric pairs
+  (electric-pair-mode)
   ;; look 'n feel
   (set-face-attribute 'default nil :height 200)
   (when (version<= "26.0.50" emacs-version)
     (global-display-line-numbers-mode)))
 (use-package eglot
+  :bind
+  (:map eglot-mode-map
+	;; code actions
+	("C-c l a" . eglot-code-actions)
+	("C-c l i" . eglot-code-action-organize-imports)
+	("C-c l q" . eglot-code-action-quickfix)
+	("C-c l e" . eglot-code-action-extract)
+	("C-c l l" . eglot-code-action-inline)
+	("C-c l r" . eglot-code-action-rewrite)
+	;; formatting
+	("C-c l f" . eglot-format-buffer)
+	;; misc
+	("C-c l v" . eglot-find-declaration)
+	;; TODO: move these to the global mode map with different binds?
+	;; diagnostics
+	("C-c l d" . eldoc)
+	("C-c l b" . flymake-show-buffer-diagnostics)
+	("C-c l p" . flymake-show-project-diagnostics))
   :config
   ;; needed for overload suggestions
   (add-to-list 'eglot-server-programs
-	       '(c++-mode . ("clangd" "--completion-style=detailed"))))
+	       '(c++-mode . ("clangd" "--completion-style=detailed")))
+  ;; setup hook for C++
+  (add-hook 'c++-mode-hook 'eglot-ensure))
 
 (use-package modus-themes
   :ensure t
@@ -35,6 +57,16 @@
   (load-theme 'modus-operandi-tinted :no-confirm)
   (setq modus-themes-to-toggle
 	'(modus-operandi-tinted modus-vivendi-tinted)))
+
+(use-package paredit
+  :ensure t
+  :config
+  (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 (use-package ace-window
   :ensure t
