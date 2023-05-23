80ba3f11a3246c14bb5bacae3c19138c3dcc773a
Brian Gluzman <bgluzman@gmail.com>
2023-05-23 18:55:09 -0400

    add use-package to default package list

--- emacs/init.el
+++ emacs/init.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(rainbow-delimiters paredit modus-themes lsp-ui lsp-mode yasnippet company magit which-key ace-window)))
+   '(use-package rainbow-delimiters paredit modus-themes lsp-ui lsp-mode yasnippet company magit which-key ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -20,6 +20,7 @@
   ;; always use electric pairs
   (electric-pair-mode)
   ;; look 'n feel
+  (setq mac-command-modifier 'control)
   (set-face-attribute 'default nil :height 200)
   (when (version<= "26.0.50" emacs-version)
     (global-display-line-numbers-mode)))
